d27ed8a27710e93f0dca7ddb5efc0d0d17af44d7
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-01-25 12:01:23 +0900

    Made the base for the game elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import words from "./wordList.json";
+import "./stylesheets/App.css";
+import Hangman from "./Hangman";
+import Keyword from "./Keyword";
+import Keyboard from "./Keyboard";
+
 function App() {
-  return <h1>Starting project</h1>;
+  const [guessWord, setGuessWord] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  console.log(guessWord);
+
+  return (
+    <div>
+      <div className='topText'>Win or Lose</div>
+      <Hangman />
+      <Keyword />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Hangman.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./stylesheets/Hangman.css";
+
+function Hangman() {
+  const hangmanHead = <div className='hangmanHead'></div>;
+  const hangmanBody = <div className='hangmanBody'></div>;
+  const hangmanRightArm = <div className='hangmanRightArm'></div>;
+  const hangmanLeftArm = <div className='hangmanLeftArm'></div>;
+  const hangmanRightLeg = <div className='hangmanRightLeg'></div>;
+  const hangmanLeftLeg = <div className='hangmanLeftLeg'></div>;
+
+  return (
+    <div className='container'>
+      {hangmanHead}
+      {hangmanBody}
+      {hangmanRightArm}
+      {hangmanLeftArm}
+      {hangmanRightLeg}
+      {hangmanLeftLeg}
+      <div className='noose'></div>
+      <div className='tophorizontal'></div>
+      <div className='vertical'></div>
+      <div className='base'></div>
+    </div>
+  );
+}
+
+export default Hangman;
